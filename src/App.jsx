5193b9d815ecc4e1cb9d71d30b17a0c404898546
Rumon360 <hmk.rumon@gmail.com>
2021-07-08 00:59:18 +0600

    Route Issue

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Nav />
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.pathname}>
-          <Route path="/capture-portfolio" exact>
+          <Route path="/" exact>
             <AboutUs />
           </Route>
           <Route path="/work" exact>
